771f28e6f77e307f30858c0061a9a3868b92ec56
OscarGlo <gloaguen.oscar@gmail.com>
2020-04-19 00:08:19 +0200

    remove shitty map gen

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -5,27 +5,6 @@ onready var buildings = $Buildings.get_children()
 export var burn_timer_max = 0.5
 var burn_timer = burn_timer_max
 
-func _ready():
-	generate_roads()
-
-func generate_roads():
-	pass
-#	var roads = []
-#	for i in range(20):
-#		roads[i] = []
-#		for j in range(20):
-#			roads[i][j] = ""
-#
-#	for i in range(4):
-#		var ax = floor(randf() * 19)
-#		var ay = floor(randf() * 19)
-#		var sx = floor(randf() * (20 - ax))
-#		var sy = floor(randf() * (20 - ay))
-#
-#		for x in range(sx):
-#			for y in range(sy):
-#				pass
-
 func _process(delta):
 	burn_timer -= delta
 	if burn_timer < 0:
